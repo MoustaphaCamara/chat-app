7356fcf77c8b5e8b784c51e3b501a4d3871eaaa1
MoustaphaCamara <cmr.mous@gmail.com>
2024-06-30 23:23:38 +0200

    feat(chat): chat color corresponding to current user

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -13,11 +13,19 @@ interface Message {
   room: string;
   author: string;
   message: string;
+  time: string;
 }
 
 const ChatBox = ({ socket, username, room }: ChatBoxProps) => {
   const [currentMessage, setCurrentMessage] = useState<string>("");
   const [messageList, setMessageList] = useState<Message[]>([]);
+
+  const handleKeyDown = (e: string) => {
+    if (e === "Enter") {
+      sendMessage();
+      setCurrentMessage("");
+    }
+  };
 
   const sendMessage = () => {
     if (currentMessage === "") return;
@@ -45,18 +53,42 @@ const ChatBox = ({ socket, username, room }: ChatBoxProps) => {
   return (
     <>
       <div>
-        <p>Body</p>
-        <div>
-          {messageList.map((message: Message, index: number) => (
-            <div key={index}>
-              <h1>{message.message}</h1>
+        <p>Current user : {username}</p>
+        {messageList.map((message: Message, index: number) => (
+          <div
+            className={`${username === message.author ? "bg-purple-900" : "bg-black"} flex items-start gap-2.5 bg-gray-950 p-4`}
+            key={index}
+          >
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://picsum.photos/200"
+              alt="Jese image"
+            />
+            <div className="flex flex-col w-full max-w-[320px] leading-1.5">
+              <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                <span className="text-sm font-semibold text-gray-900 dark:text-white">
+                  {message.author}
+                </span>
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  {message.time}
+                </span>
+              </div>
+              <p className="text-sm font-normal py-2 text-gray-900 dark:text-white">
+                {message.message}
+              </p>
+              <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                Delivered
+              </span>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+
         <InputText
+          value={currentMessage}
           label="Chat box"
           placeholder="message content"
           onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e.key)}
         />
         <Button content="&#9658; Envoyer" onClick={sendMessage} />
       </div>
