dc5fc48a6b0ea4f68c04f1b2126d367ee5956036
MoustaphaCamara <cmr.mous@gmail.com>
2024-06-30 17:58:06 +0200

    chore(packages): init project, install express & socket-io

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const port = 8000;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User logged in as ${socket.id}.`);
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} logged out.`);
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port : ${port}`);
+});
